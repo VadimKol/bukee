0f9e1f9d8d808bd5189fb1d7b0f0fefe66bfee9e
VadimKol <47818256+VadimKol@users.noreply.github.com>
2019-04-22 02:03:43 +0300

    blocks contents and books

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 import "./import/modules.js";
 import "./import/components.js";
+
+$(document).ready(function(){
+    //no js
+    //no js
+    $(".toc-list").children().slice(10).hide();
+    $(".toc__btn").show();
+    $(".page-contents").css("padding","143px 0 256px");
+    $(".toc__btn").click(function(){
+        $(".toc-list").children().slice(10).show();
+        $(".page-contents").css("padding","143px 0 144px");
+        $(this).hide();
+    });
+});
